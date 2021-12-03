3b4ca786de43b08da4f69981acc5488fad47e94a
talalter <64363268+talalter@users.noreply.github.com>
2021-12-03 22:54:23 +0200

    Update Cluster.java

--- main/java/bgu/spl/mics/application/objects/Cluster.java
+++ main/java/bgu/spl/mics/application/objects/Cluster.java
@@ -1,22 +1,29 @@
 package bgu.spl.mics.application.objects;
-
+import java.util.*;
 
 /**
  * Passive object representing the cluster.
  * <p>
  * This class must be implemented safely as a thread-safe singleton.
  * Add all the fields described in the assignment as private fields.
- * Add fields and methods to this class as you see fit (including public methods and constructors).
+ * Add fields and methoֺds to this class as you see fit (including public methods and constructors).
  */
 public class Cluster {
-
+	Set<GPU> gpuSet;
+	Set<CPU> cpuSet;
+	private static class ClusterHolder {
+		private static Cluster instance = new Cluster();
+	}
+	private Cluster(){
+		this.gpuSet = new HashSet<GPU>();
+		this.cpuSet = new HashSet<CPU>();
+	}
 
 	/**
      * Retrieves the single instance of this class.
      */
 	public static Cluster getInstance() {
-		//TODO: Implement this
-		return null;
+		return ClusterHolder.instance;
 	}
 
 }
